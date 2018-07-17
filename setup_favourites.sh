2154b0e331295e3a614231a31cde90cd14c3baa0
Szymon Balicki <s.z.balicki@gmail.com>
2018-07-17 16:32:41 +0200

    Add mater terminal custom config
    Signed-off-by: Szymon Balicki <s.z.balicki@gmail.com>

--- setup_favourites.sh
+++ setup_favourites.sh
@@ -28,3 +28,11 @@ sudo apt install chromium chromium-l10n vlc gparted eclipse aptitude dconf-edito
 gsettings set org.mate.caja.desktop home-icon-visible false
 gsettings set org.mate.caja.desktop trash-icon-visible false
 gsettings set org.mate.caja.desktop volumes-visible false
+
+################ Set terminal prefs ################
+gsettings set org.mate.terminal.profiles.default custom-command zsh
+gsettings set org.mate.terminal.profiles.default use-custom-command true
+gsettings set org.mate.terminal.profiles.default bold-color #000000000000
+gsettings set org.mate.terminal.profiles.default foreground-color #FFFFFFFFFFFF
+gsettings set org.mate.terminal.profiles.default use-custom-colors true
+gsettings set org.mate.terminal.profiles.default use-theme-colors false
